376e1b5387b78405d9ac96a4626d8b4bfedfd3b5
Nathan Hoekstra <nathan.hoekstra@student.hu.nl>
2019-05-07 15:30:53 +0200

    Added pyfirmata test

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 import cv2
+from pyfirmata import Arduino, util
+import platform
+import time
+
+# if platform is macOS and board is Arduino DUE
+if platform.system() == "Darwin":
+    board = Arduino('/dev/cu.usbmodem14101')
 
 
-img = cv2.imread('img/lena.png',1)
-cv2.imshow('image',img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+def main():
+    img = cv2.imread('img/lena.png',1)
+    cv2.imshow('image',img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
+
+if __name__ == "__main__":
+    while True:
+        board.digital[13].write(1)
+        time.sleep(1)
+        board.digital[13].write(0)
+        time.sleep(0.5)
