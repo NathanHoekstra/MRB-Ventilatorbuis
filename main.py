23cda9d29f9dccbe8d28279136fcc1898a1aba6e
Nathan Hoekstra <nathan.hoekstra@student.hu.nl>
2019-05-14 14:16:52 +0200

    Added working code for detecting the ball

--- main.py
+++ main.py
@@ -1,24 +1,61 @@
 import cv2
 import time
 from arduinoHandler import arduinoHandler
+import imutils
 
 PWM_3 = 'd:3:p'
 LED_BUILTIN = 13
 
+ballLower = (8, 130, 200)
+ballUpper = (255, 255, 255)
+
 def main():
-    img = cv2.imread('img/lena.png',1)
-    cv2.imshow('image',img)
-    cv2.waitKey(0)
+    cap = cv2.VideoCapture(0)
+
+    while True:
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+
+        # Our operations on the frame come here
+        blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+        hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+
+        mask = cv2.inRange(hsv, ballLower, ballUpper)
+        mask = cv2.erode(mask, None, iterations=2)
+        mask = cv2.dilate(mask, None, iterations=2)
+
+        cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+                                cv2.CHAIN_APPROX_SIMPLE)
+        cnts = imutils.grab_contours(cnts)
+
+        # only proceed if at least one contour was found
+        if len(cnts) > 0:
+            # find the largest contour in the mask, then use
+            # it to compute the minimum enclosing circle and
+            # centroid
+            c = max(cnts, key=cv2.contourArea)
+
+            ((x, y), radius) = cv2.minEnclosingCircle(c)
+
+            M = cv2.moments(c)
+            center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+
+            # only proceed if the radius meets a minimum size
+            if radius > 10:
+                # draw the circle and centroid on the frame,
+                # then update the list of tracked points
+                cv2.circle(frame, (int(x), int(y)), int(radius),
+                           (0, 255, 255), 2)
+                #cv2.circle(frame, center, 5, (0, 0, 255), -1)
+
+        # Display the resulting frame
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # When everything done, release the capture
+    cap.release()
     cv2.destroyAllWindows()
 
-
-def blink():
-    due = arduinoHandler()
-    while True:
-        due.digitalWrite(LED_BUILTIN, True)
-        time.sleep(1)
-        due.digitalWrite(LED_BUILTIN, False)
-        time.sleep(0.5)
-
 if __name__ == "__main__":
-    blink()
+    main()
