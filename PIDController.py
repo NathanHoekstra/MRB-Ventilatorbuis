85852307e0c8298c54b8f6bfe26f9605281f9ec6
MarcDirven <marc.dirven@student.hu.nl>
2019-05-21 14:59:01 +0200

    Added PID Controller class

---
+++ PIDController.py
@@ -0,0 +1,21 @@
+from ArduinoHandler import ArduinoHandler
+
+class PIDController:
+    def __getError(self):
+        return self.setPoint - self.ballPosition[1]
+
+    def __init__(self, Kp : int, Ki : int, Kd : int, pwmPin : int):
+        self.arduino = ArduinoHandler()
+        self.ballPosition = None
+        self.setPoint = 250
+        self.Kp = Kp
+        self.Ki = Ki
+        self.Kd = Kd
+        self.pwmPin = pwmPin
+
+    def setBallPosition(self, ballPosition):
+        self.ballPosition = ballPosition
+
+    def setSetpoint(self, setPoint):
+        self.setPoint = setPoint
+
--- main.py
+++ main.py
@@ -1,32 +1,33 @@
 import cv2
-import time
-from ArduinoHandler import ArduinoHandler
 from BallDetector import  BallDetector
 from ObjectTracker import ObjectTracker
+from PIDController import *
 
-PWM_3 = 'd:3:p'
-LED_BUILTIN = 13
-
-ballLower = (8, 130, 200)
-ballUpper = (255, 255, 255)
-
-ballDetector = BallDetector(ballLower, ballUpper)
-objectTracker = ObjectTracker()
 
 def main():
+    ballLower = (8, 130, 200)
+    ballUpper = (255, 255, 255)
+
+    ballDetector = BallDetector(ballLower, ballUpper)
+    objectTracker = ObjectTracker()
+    pidController = PIDController(1, 1, 1, 20)
+
+    pidController.setSetpoint(250)
+
     print("Program started, press q to quit the application")
     print("When using the ObjectTracker, press s to freeze a frame and use the mouse to select an object")
+
     cap = cv2.VideoCapture(0)
 
     while True:
         # Capture frame-by-frame
         ret, frame = cap.read()
 
-        #result = ballDetector.locateBall(frame)
-        objectTracker.trackObject(frame)
-
+        frame = ballDetector.locateBall(frame)
+        #objectTracker.trackObject(frame)
+        pidController.setBallPosition(ballDetector.ballPosition)
         # Display the resulting frame
-        #cv2.imshow('frame', result)
+        cv2.imshow('frame', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
