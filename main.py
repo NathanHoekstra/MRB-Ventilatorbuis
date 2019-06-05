8e7f6e9754004daffdc33ccc0fd2586c8564de39
MarcDirven <marc.dirven@student.hu.nl>
2019-06-05 13:50:43 +0200

    added mouse click follow event

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import cv2
 
-import time
-
 from WindowManager import WindowManager
 from BallDetector import  BallDetector
 from ObjectTracker import ObjectTracker
 from PIDController import *
-
 from ArduinoHandler import ArduinoHandler
+
+mouseClickPosition = (100, 300)
+
+def onMouseClickEvent(event, x, y, flags, params):
+    global mouseClickPosition
+    if event == cv2.EVENT_LBUTTONDBLCLK:
+        mouseClickPosition = (x, y)
 
 def main():
     ballLower = (8, 130, 160)
@@ -17,12 +21,13 @@ def main():
     ballDetector = BallDetector(ballLower, ballUpper)
     objectTracker = ObjectTracker()
     nano = ArduinoHandler()
-    pidController = PIDController(1, 1, 1, 20, nano)
+    pidController = PIDController(1, 1, 2, 20, nano)
     nano.setAnalogPort('d:3:p')
 
     print("Program started, press q to quit the application")
     print("When using the ObjectTracker, press s to freeze a frame and use the mouse to select an object")
     cap = cv2.VideoCapture(0)
+
 
     cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M','J','P','G'))
     cap.set(cv2.CAP_PROP_FPS, 60)
@@ -33,18 +38,18 @@ def main():
         frame = windowManager.resizeWindow(frame, width=800)
         fps = cap.get(cv2.CAP_PROP_FPS)
         height, width = frame.shape[:2]
+
         windowTitle = "Object detection W: " + str(width) + " H: " + str(height) + " FPS: " + str(fps)
+        cv2.setMouseCallback(windowTitle, onMouseClickEvent)
 
         frame = ballDetector.locateBall(frame)
         objectTracker.trackObject(frame, windowTitle)
 
         if objectTracker.isSelected():
-            pidController.controlPIDFan(ballDetector.getBallPosition(),
-                                        objectTracker.getObjectPosition(), fps)
+            pidController.controlPIDFan(ballDetector.getBallPosition(), objectTracker.getObjectPosition(), fps)
 
         elif ballDetector.isBallFound() and not objectTracker.isSelected():
-            pidController.controlPIDFan(ballDetector.getBallPosition(),
-                                        windowManager.midPoint, fps)
+            pidController.controlPIDFan(ballDetector.getBallPosition(), mouseClickPosition, fps)
         else:
             nano.analogWrite(0.8)
 
